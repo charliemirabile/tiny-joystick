710d128dd5713eff85753c9da7bd3f968a5e9800
NAME <>
2021-02-20 16:27:29 -0500

    even more hardcoding

--- main.c
+++ main.c
@@ -343,15 +343,15 @@ midimsg;
 
 
 
-midimsg lookuptable[] = {
-/*	(midimsg){.codeindex=0xB, .channel=0, .msg_type=0xB, .controller=100, .value=100},
+/*midimsg lookuptable[] = {
+	(midimsg){.codeindex=0xB, .channel=0, .msg_type=0xB, .controller=100, .value=100},
 	(midimsg){.codeindex=0xB, .channel=0, .msg_type=0xB, .controller=101, .value=100},
 	(midimsg){.codeindex=0xB, .channel=0, .msg_type=0xB, .controller=102, .value=100},
 	(midimsg){.codeindex=0xB, .channel=0, .msg_type=0xB, .controller=103, .value=100},
 	(midimsg){.codeindex=0xB, .channel=0, .msg_type=0xB, .controller=100, .value=0},
 	(midimsg){.codeindex=0xB, .channel=0, .msg_type=0xB, .controller=101, .value=0},
 	(midimsg){.codeindex=0xB, .channel=0, .msg_type=0xB, .controller=102, .value=0},
-	(midimsg){.codeindex=0xB, .channel=0, .msg_type=0xB, .controller=103, .value=0},*/
+	(midimsg){.codeindex=0xB, .channel=0, .msg_type=0xB, .controller=103, .value=0},
 	(midimsg){.bytes={0xB0, 0x0B, 100, 100}},
 	(midimsg){.bytes={0xB0, 0x0B, 101, 100}},
 	(midimsg){.bytes={0xB0, 0x0B, 102, 100}},
@@ -360,6 +360,17 @@ midimsg lookuptable[] = {
 	(midimsg){.bytes={0xB0, 0x0B, 101, 0}},
 	(midimsg){.bytes={0xB0, 0x0B, 102, 0}},
 	(midimsg){.bytes={0xB0, 0x0B, 103, 0}},
+};*/
+
+uchar lookuptable[][4] = {
+	{0xB0, 0x0B, 100, 100},
+	{0xB0, 0x0B, 101, 100},
+	{0xB0, 0x0B, 102, 100},
+	{0xB0, 0x0B, 103, 100},
+	{0xB0, 0x0B, 100, 0},
+	{0xB0, 0x0B, 101, 0},
+	{0xB0, 0x0B, 102, 0},
+	{0xB0, 0x0B, 103, 0},
 };
 
 
@@ -400,7 +411,7 @@ void main(void)
 				else
 					move=pos-1;
 				last_pos = pos;
-				usbSetInterrupt(lookuptable[move].bytes,sizeof(midimsg));
+				usbSetInterrupt(lookuptable[move],4);
 			}
 		}
 
